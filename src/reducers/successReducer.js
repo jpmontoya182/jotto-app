2959f560972e78d7bd14b81378edf2744774944e
Juan P. Montoya <Juan.Montoya@viamericas.com>
2020-06-30 08:58:35 -0500

    it was added the test to successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,15 @@
-export default (state, action) => {
-    return null;
-}
+import { actionsTypes } from "../actions";
+/**
+ * state : Array of guessed words
+ * action: to be reduced
+ * @returns {boolean} : new success state
+ */
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionsTypes.CORRECT_GUESS:
+      return true;
+
+    default:
+      return state;
+  }
+};
